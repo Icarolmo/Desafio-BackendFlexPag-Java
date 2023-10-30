609d3319d69a12b2c1bd5a0eae50160b81663128
Icarus <icarolmol@gmail.com>
2023-10-30 18:03:40 -0300

    Fix: implemented service with business rules and CRUD endpoints.

--- src/main/java/com/flexpag/paymentscheduler/controller/SchedulingController.java
+++ src/main/java/com/flexpag/paymentscheduler/controller/SchedulingController.java
@@ -1,13 +1,16 @@
 package com.flexpag.paymentscheduler.controller;
 
+import com.flexpag.paymentscheduler.dto.RequestScheduling;
 import com.flexpag.paymentscheduler.service.SchedulingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+@Slf4j
 @RestController
 @RequestMapping("/scheduling")
 public class SchedulingController {
@@ -15,9 +18,35 @@ public class SchedulingController {
     @Autowired
     SchedulingService service;
 
-
-    @GetMapping @ExceptionHandler
+    @GetMapping
     public ResponseEntity getAllScheduling(){
+        log.info("Chamando servico de consulta agendamentos: getAllScheduling");
         return ResponseEntity.ok().body(service.getAllScheduling());
     }
+
+    @GetMapping(params = "id")
+    public ResponseEntity getSchedulingById(@RequestParam(name = "id") UUID id){
+        log.info("Chamando servico de consulta agendamentos por id: getSchedulingById");
+        return ResponseEntity.ok().body(service.getSchedulingById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity createNewScheduling(@RequestBody @Validated RequestScheduling data){
+        log.info("Chamando servico de criação de agendamentos: createNewScheduling");
+        return ResponseEntity.ok().body(service.createNewScheduling(data));
+    }
+
+    @PutMapping
+    public ResponseEntity updateScheduling(@RequestParam(name = "id") UUID id, @RequestBody @Validated RequestScheduling data){
+        log.info("Chamando servico de alteração de agendamentos: updateScheduling");
+        return ResponseEntity.ok().body(service.updateScheduling(id, data));
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteScheduling(@RequestParam(name = "id") UUID id){
+        log.info("Chamando servico de exclusão de agendamentos: deleteScheduling");
+        service.deleteScheduling(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/flexpag/paymentscheduler/service/SchedulingService.java
+++ src/main/java/com/flexpag/paymentscheduler/service/SchedulingService.java
@@ -1,18 +1,66 @@
 package com.flexpag.paymentscheduler.service;
 
+import com.flexpag.paymentscheduler.dto.RequestScheduling;
 import com.flexpag.paymentscheduler.dto.SchedulingDTO;
+import com.flexpag.paymentscheduler.entity.Scheduling;
 import com.flexpag.paymentscheduler.repository.SchedulingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class SchedulingService {
 
     @Autowired
-    SchedulingRepository repository;
+    private SchedulingRepository repository;
 
-    public SchedulingDTO getAllScheduling(){
-        return  (SchedulingDTO) repository.findAll();
+    @Autowired
+    private EntityManager entityManager;
+
+    public List<SchedulingDTO> getAllScheduling(){
+        List<SchedulingDTO> allSchedulings = new ArrayList<>();
+        for(Scheduling databaseScheduling : repository.findAll()){
+            allSchedulings.add(new SchedulingDTO(databaseScheduling));
+        }
+        return allSchedulings;
     }
+
+    public SchedulingDTO getSchedulingById(UUID id){
+        Optional<Scheduling> transferData = repository.findById(id);
+        if(transferData.isPresent()){
+            return new SchedulingDTO(
+                    transferData.get().getId(),
+                    transferData.get().getStatus(),
+                    transferData.get().getDate(),
+                    transferData.get().getAmount());
+        } else {
+            return null;
+        }
+    }
+
+    public SchedulingDTO createNewScheduling(RequestScheduling data){
+        return new SchedulingDTO(repository.save(new Scheduling(data)));
+    }
+
+    public SchedulingDTO updateScheduling(UUID id, RequestScheduling data){
+        Scheduling alteringScheduling = repository.getReferenceById(id);
+
+        alteringScheduling.setDate(data.getDate());
+        alteringScheduling.setAmount(data.getAmount());
+
+        entityManager.merge(alteringScheduling);
+
+        return new SchedulingDTO(alteringScheduling);
+    }
+
+    public void deleteScheduling(UUID id){
+        repository.deleteById(id);
+    }
+
 
 }
