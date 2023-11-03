5686331d528628e98cfca6456569464a1786dc5c
Icarus <icarolmol@gmail.com>
2023-11-03 19:10:24 -0300

    Refactor: add findById and findAll in repository body.

--- src/main/java/com/flexpag/paymentscheduler/repository/SchedulingRepository.java
+++ src/main/java/com/flexpag/paymentscheduler/repository/SchedulingRepository.java
@@ -1,8 +1,19 @@
 package com.flexpag.paymentscheduler.repository;
 
+import com.flexpag.paymentscheduler.dto.SchedulingDTO;
 import com.flexpag.paymentscheduler.entity.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SchedulingRepository extends JpaRepository<Scheduling, UUID> {
+
+    Optional<Scheduling> findById(UUID id);
+
+    List<Scheduling> findAll();
 }
